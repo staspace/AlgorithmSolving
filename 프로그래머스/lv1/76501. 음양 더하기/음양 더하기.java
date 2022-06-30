e69f9f945247414aa7f8187b8fb04f553a767d86
Sanghoon, Song <99252977+staspace@users.noreply.github.com>
2022-06-30 11:55:01 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int result = 0;
+        for(int i = 0; i<absolutes.length; i++){
+            if(signs[i] == false){
+                absolutes[i] *= -1;
+            }
+            result += absolutes[i];
+        }
+        return result;
+    }
+}
