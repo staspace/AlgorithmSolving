180bcbfee2ccfcae125d6aa0f9f790a6c9de806a
Sanghoon, Song <99252977+staspace@users.noreply.github.com>
2022-07-02 07:52:01 +0900

    [Bronze IV] Title: 주사위 세개, Time: 108 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -10,7 +10,7 @@ public class Main {
 
 		if (a == b && b == c)
 			System.out.println(10000 + a * 1000);
-		else if ((a == b || a == c))
+		else if (a == b || a == c)
 			System.out.println(1000 + a * 100);
 		else if (b == c)
 			System.out.println(1000 + b * 100);
