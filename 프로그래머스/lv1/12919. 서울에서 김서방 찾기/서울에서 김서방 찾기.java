1624034437760ddac2679277ccd67d9d557f7c40
Sanghoon, Song <99252977+staspace@users.noreply.github.com>
2022-07-02 09:10:28 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.81 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution {
+    private static String answer = "";
+    public String solution(String[] seoul) {
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].contains("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            } 
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(answer);
+    }
+}
