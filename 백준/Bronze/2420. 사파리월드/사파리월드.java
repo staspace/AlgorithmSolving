a6fa632fdf1cfbc06a35ddbdb84144fb7ea0357d
Sanghoon, Song <99252977+staspace@users.noreply.github.com>
2022-07-04 08:43:05 +0900

    [Bronze V] Title: 사파리월드, Time: 108 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.java
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		long N = sc.nextInt();
-		long M = sc.nextInt();
+		long N = sc.nextLong();
+		long M = sc.nextLong();
 		sc.close();
 		
 		System.out.println(Math.abs(N - M));
