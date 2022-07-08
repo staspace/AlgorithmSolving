2706e27cc4596b954f45966dd940f15bc6b67240
Sanghoon, Song <99252977+staspace@users.noreply.github.com>
2022-07-08 10:00:48 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 13004 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] chess = { 1, 1, 2, 2, 2, 8 };
+
+		int[] arr = new int[6];
+
+		for (int i = 0; i < 6; i++) {
+			arr[i] = chess[i] - sc.nextInt();
+		}
+		sc.close();
+
+		for (int i = 0; i < 6; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
